be5167975a742a62da622a98f907ef971b25c99a
booksun54 <598437682@qq.com>
2018-06-03 18:00:20 +0800

    添加闪屏页面

---
+++ app/src/main/java/com/example/booksun/onenews/activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.booksun.onenews.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.booksun.onenews.R;
+
+public class SplashActivity extends Activity {
+    protected Handler mHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },2000);
+    }
+}
